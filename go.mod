a7f177cf84652eadf41269d0eaf84b5ee3b695c0
Alexander Yurkov <alexander.v.yurkov@gmail.com>
2024-12-17 18:00:07 +0000

    feat: Add "is" with replacement module.
    Add the smaller version of the "is" module for testing purposes of the
    replacement module.
    Update go.mod so that go uses the local version of "is" and not the
    github version.
    Update helloworld.go so that go can compile and run.
    
    Signed-off-by: Alexander Yurkov <alexander.v.yurkov@gmail.com>

--- go.mod
+++ go.mod
@@ -1,5 +1,10 @@
-module github.com/OSS-Keepers/recipetool-go-test/tree/feature/hello-world-program
+module github.com/OSS-Keepers/recipetool-go-test
 
 go 1.18
 
-require github.com/godbus/dbus/v5 v5.1.0
+replace github.com/matryer/is => ./is
+
+require (
+	github.com/godbus/dbus/v5 v5.1.0
+	github.com/matryer/is v1.4.1
+)
--- helloworld.go
+++ helloworld.go
@@ -3,10 +3,30 @@ package main
 import (
     "fmt"
     "os"
+    "testing"
 
+    "github.com/matryer/is"
     "github.com/godbus/dbus/v5"
 )
 
 func main() {
+    // dbus and os dependency usage
+    conn, err := dbus.ConnectSessionBus()
+    if err != nil {
+        fmt.Fprintln(os.Stderr, "Failed to connect to session bus:", err)
+        os.Exit(1)
+    }
+    defer conn.Close()
+
+    // print hello world
     fmt.Println("Hello World!")
-} 
+}
+
+// "is" dependency usage
+func TestExample(t *testing.T) {
+    is := is.New(t)
+
+    is.True(true) // This test passes
+    is.Equal(1, 1) // This test passes
+    is.NoErr(nil) // No error
+}
